a2eb074cc09aa1c4fa7cf92892875fafc9d45225
Britton Walker <bwalker1801@gmail.com>
2024-04-11 12:32:59 -0400

    Adds plugins and alias js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
 const { getWebpackEntryPoints } = require("@wordpress/scripts/utils/config");
+const path = require("path");
 
 module.exports = {
 	...defaultConfig,
 	entry: {
 		...getWebpackEntryPoints(),
-		tktkStyles: "./src/tktk-styles/index.js",
+		"tktk-blocks-styles": "./src/tktk-styles/index.js",
+		"tktk-blocks-plugins": "./src/plugins/index.js",
 	},
 	resolve: {
 		...defaultConfig.resolve,
+		alias: {
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@plugins": path.resolve(__dirname, "./src/plugins"),
+		},
 	},
 	module: {
 		...defaultConfig.module,
